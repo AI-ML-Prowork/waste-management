bbce0eccae91d6e25be83d752fdb614b5197a636
CFC-Tech-AI <pythoncfc@gmail.com>
2024-02-23 14:24:46 +0530

    first commit

--- food_management/views.py
+++ food_management/views.py
@@ -7,6 +7,7 @@ from django.contrib import messages
 from django.contrib.auth.models import User
 from django.contrib.auth import login, authenticate
 import os
+import io
 import base64
 import barcode
 from barcode.writer import ImageWriter
